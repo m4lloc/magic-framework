257938cd0d73bda1d86295c24642e8a5e45197a5
Sander van Rossum <sander@sandervanrossum.nl>
2017-03-07 09:43:33 +0100

    View wrapper for Smarty

--- src/lib/view.php
+++ src/lib/view.php
@@ -2,6 +2,50 @@
 
   namespace Magic;
 
-  class View {
+	class View {
+		
+		private $smarty = null;
+    private static $instance = null;
 
-  }
+		public function __set($name, $value) {
+			$this->$name = $value;
+			$this->assign($name, $this->$name);
+		}
+
+		public function &__get($name) {
+			return $this->$name;
+		}
+
+		public function __construct() {
+			if(null === $this->smarty) {
+				$this->smarty = new Smarty();
+			}
+		}
+
+		public static function instance() {
+			if(!isset(self::$instance)) {
+				self::$instance = new self();
+			}
+			return self::$instance;
+		}
+
+		public static function setTemplateDir($path) {
+			return self::instance()->smarty->setTemplateDir($path);
+		}
+
+		public static function setCompileDir( $path ) {
+			return self::instance()->smarty->setCompileDir($path);
+		}
+
+		public static function assign( $name, $value ) {
+			return self::instance()->smarty->assign($name, $value);
+		}
+
+		public static function fetch( $tpl ) {
+			return self::instance()->smarty->fetch($tpl);
+		}
+
+		public static function display( $tpl ) {
+			return self::instance()->smarty->display($tpl);
+		}
+	}
