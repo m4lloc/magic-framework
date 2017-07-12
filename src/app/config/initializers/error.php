c63432837e626a30516c57a5da51286fb9ff598c
Sander van Rossum <sander@sandervanrossum.nl>
2017-07-12 23:27:32 +0200

    Send errors to Sentry

--- src/app/config/initializers/error.php
+++ src/app/config/initializers/error.php
@@ -3,11 +3,13 @@
   class error extends \Magic\Initializer {
 
     public function init() {
-      $client = new Raven_Client(getenv('SENTRY_DSN'));
-
-      $error_handler = new Raven_ErrorHandler($client);
-      $error_handler->registerExceptionHandler();
-      $error_handler->registerErrorHandler();
-      $error_handler->registerShutdownFunction();
+      if(getenv('MAGIC_SENTRY_ENABLED') == 'true') {
+        $client = new Raven_Client(getenv('SENTRY_DSN'));
+        
+        $handler = new Raven_ErrorHandler($client);
+        $handler->registerExceptionHandler();
+        $handler->registerErrorHandler();
+        $handler->registerShutdownFunction();
+      }
     }
   }
