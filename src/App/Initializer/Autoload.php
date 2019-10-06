31e26fd76d62a0f2aeb5b5516d73acd8ccdd5239
m4lloc <sander@sandervanrossum.nl>
2019-10-06 14:14:28 +0200

    autoload from project directory

--- src/App/Initializer/Autoload.php
+++ src/App/Initializer/Autoload.php
@@ -5,12 +5,31 @@
   class Autoload extends \M\Initializer {
 
     public function init() {
-      set_include_path(implode(PATH_SEPARATOR,[
+      $paths = array_filter([
         get_include_path(),
         realpath('../src/App'),
-        realpath('../src/Lib')
-      ]));
+        realpath(dirname(__FILE__, 2)),
+        realpath('../src/Lib'),
+        realpath(dirname(__FILE__, 3) .'/Lib'),
+      ]);
+      set_include_path(implode(PATH_SEPARATOR, $paths));
 
+      $this->registerAutoloader();
+      $this->registerDecoraterLoader();
+    }
+
+    private function registerAutoloader() {
+      spl_autoload_register(function($namespaced_class) {
+        if(class_exists($namespaced_class)) { return true; }
+
+        $path = realpath(\M\App::$VENDOR_DIR .'/../src/App/'. substr(str_replace('\\', '/', $namespaced_class), 2) .'.php');
+        if(file_exists($path)) {
+          require $path;
+        }
+      }, true, true);
+    }
+
+    private function registerDecoraterLoader() {
       spl_autoload_register(function($namespaced_trait) {
         if(trait_exists($namespaced_trait)) { return true; }
         if(substr($namespaced_trait, -9) != 'Decorator') { return false; }
